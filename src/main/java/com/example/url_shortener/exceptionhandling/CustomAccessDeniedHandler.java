616f04c5577d6bb2402386c2ea4ff32420f769c9
vasilev-anri <vasilev.anri75@gmail.com>
2025-04-02 05:44:06 +0300

    Improved custom access denied error response with ISO 8601 timestamp format

--- src/main/java/com/example/url_shortener/exceptionhandling/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/url_shortener/exceptionhandling/CustomAccessDeniedHandler.java
@@ -1,5 +1,7 @@
 package com.example.url_shortener.exceptionhandling;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,22 +11,37 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAccessDeniedHandler() {
+        this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        LocalDateTime currentTimeStamp = LocalDateTime.now();
-        String message = (accessDeniedException != null && accessDeniedException.getMessage() != null) ? accessDeniedException.getMessage() : "Authorization failed";
         String path = request.getRequestURI();
+        String currentTimeStamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
 
-        response.setHeader("error-reason", "Authorization failed");
+
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setContentType("application/json; charset=utf-8");
 
-        String jsonResponse =
-                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
-                        currentTimeStamp, HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase(),
-                        message, path);
-        response.getWriter().write(jsonResponse);
+        Map<String , Object> errorResponse = new HashMap<>();
+
+        errorResponse.put("timestamp", currentTimeStamp);
+        errorResponse.put("status", HttpStatus.FORBIDDEN.value());
+        errorResponse.put("error", HttpStatus.FORBIDDEN.getReasonPhrase() + " - " + accessDeniedException.getMessage());
+        errorResponse.put("message", "PREMIUM Role Required To Create Custom Short URLs");
+        errorResponse.put("path", path);
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
