6a0c6646dfbd4874be8f35ede5ff96485d9f7d5e
vasilev-anri <vasilev.anri75@gmail.com>
2025-04-13 10:18:25 +0300

    Add ExceptionHandler for UsernameExistsException

--- src/main/java/com/example/url_shortener/exceptionhandling/GlobalExceptionHandler.java
+++ src/main/java/com/example/url_shortener/exceptionhandling/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.example.url_shortener.exceptionhandling;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,6 +16,14 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleResponseStatusException(ResponseStatusException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(e.getStatusCode(), e.getReason());
         problemDetail.setTitle("Validation Error");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(UsernameExistsException.class)
+    public ProblemDetail handleUsernameExistsException(UsernameExistsException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(e.getStatusCode(), e.getReason());
+        problemDetail.setTitle("Duplicate Username");
+        problemDetail.setProperty("timestamp", LocalDateTime.now());
         return problemDetail;
     }
 
