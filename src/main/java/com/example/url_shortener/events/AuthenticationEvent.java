36895203f74a94f0031ec3fdfc7e98204726b039
vasilev-anri <vasilev.anri75@gmail.com>
2025-04-17 13:14:12 +0300

    add authentication event

--- src/main/java/com/example/url_shortener/events/AuthenticationEvent.java
+++ src/main/java/com/example/url_shortener/events/AuthenticationEvent.java
@@ -1,24 +1,23 @@
-//package com.example.url_shortener.events;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
-//import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@Slf4j
-//public class AuthenticationEvent {
-//
-//    @EventListener
-//    public void onSuccess(AuthenticationSuccessEvent successEvent) {
-//        log.info("Authentication success for user: {}", successEvent.getAuthentication().getName());
-//    }
-//
-//    @EventListener
-//    public void onFailure(AbstractAuthenticationFailureEvent failureEvent) {
-//        log.info("Authentication failed for user: {} due to {}", failureEvent.getAuthentication().getName(),
-//                failureEvent.getException().getMessage());
-//    }
-//
-//}
+package com.example.url_shortener.events;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AuthenticationEvent {
+
+    @EventListener
+    public void onSuccess(AuthenticationSuccessEvent successEvent) {
+        log.info("Authentication success for user: {}", successEvent.getAuthentication().getName());
+    }
+
+    @EventListener
+    public void onFailure(AbstractAuthenticationFailureEvent failureEvent) {
+        log.info("Authentication failed. {} ", failureEvent.getException().getMessage());
+    }
+
+}
